79b63c2c0d1ef656c821f26a4815393df8071f8e
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-01-20 12:09:41 +0000

    tidied up css, made more universal across site changed added photos to pages 1&2

--- test.js
+++ test.js
@@ -75,7 +75,3 @@ function sketch(event){
   ctx.stroke();
 }
 
-document.getElementById('play').addEventListener('click', function (e) {
-    e.preventDefault();
-    document.getElementById('audio').play();
-  });
